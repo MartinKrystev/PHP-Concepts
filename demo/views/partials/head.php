045a97cc02db6fa8e282ee5be087d0977122425e
MartinKrystev <martin.krystev@gmail.com>
2025-03-16 23:02:54 +0200

    Partials head

---
+++ demo/views/partials/head.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+    <script src="https://cdn.tailwindcss.com?pluins=forms"></script>
+</head>
+<body>
+
+<div class="min-h-full">
