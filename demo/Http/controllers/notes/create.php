4306c2957790aba3ca3339ae80d66d833d945b26
MartinKrystev <martin.krystev@gmail.com>
2025-04-14 23:20:36 +0300

    routes

---
+++ demo/Http/controllers/notes/create.php
@@ -0,0 +1,14 @@
+<?php
+
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+$errors = [];
+
+view("/notes/create.view.php", [
+    'heading' => 'Create Note',
+    'errors' => $errors
+]);
+
